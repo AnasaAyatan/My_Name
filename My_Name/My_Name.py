54e0caa4c7812c7b97004c13272655b1617158f4
AnasaAyatan <144476071+AnasaAyatan@users.noreply.github.com>
2023-09-14 17:06:57 -0300

    Add files via upload

---
+++ My_Name/My_Name.py
@@ -0,0 +1,9 @@
+#My real name is "Valentinez Alkalinella Xifax Sicidabohertz Gumbigobilla Blue Stradivari Talentrent Pierre Andres Charton-Haymoss Ivanovicci Baldeus George Doitzel Kaiser III"! Also known as Vash! Now, repeat after me! This world is made of love and peace! love and peace! love and peace!
+
+firstName = "John"
+
+middleName = "Valentinez Alkalinella Xifax Sicidabohertz Gumbigobilla Blue Stradivari Talentrent Pierre Andres Charton-Haymoss Ivanovicci Baldeus George Doitzel Kaiser III"
+
+lastName = "Ayatan"
+
+print ("My full name is " + firstName +" "+ middleName +" "+ lastName + "!")
